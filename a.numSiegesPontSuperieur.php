437b52a9ea2e408d419fe5cce7698d5414b433e7
melisa-suciu <144272179+melisa-suciu@users.noreply.github.com>
2023-11-22 16:34:38 +0100

    Add files via upload

---
+++ a.numSiegesPontSuperieur.php
@@ -0,0 +1,9 @@
+<?php
+		echo "Sièges du pont supérieur:\n\n" ;
+	
+			for( $numSiege = 30 ; $numSiege <= 39 ; $numSiege = $numSiege +1){
+				
+					echo "\tSiège numéro " , $numSiege , "\n" ;
+			}
+	
+?>
---
+++ b.numSiegesPontEmbarquementPour.php
@@ -0,0 +1,11 @@
+<?php
+	
+	echo "Siège du pont d'embarquement : \n\n" ;
+	
+	for ( $numSiege = 20 ; $numSiege <= 29 ; $numSiege = $numSiege + 1 ) {
+		
+		echo "\tSiège numéro : " , $numSiege ,"\n" ;
+	}
+	
+
+?>
